b553dda06e3317a53c882b42db3530811eb02b2a
yangxiaoping <2878847069@qq.com>
2024-05-21 18:47:37 +0800

    feat: 排除HTML

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,13 @@ module.exports = {
     workboxPluginMode: 'GenerateSW',
     // workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      // swSrc: 'src/service-worker.js'
+      // swSrc: 'src/service-worker.js',
       // 排除掉html的缓存
-      // exclude: [/\.map$/, /^manifest.*\.js$/, /\.html$/]
+      exclude: [/\.map$/, /^manifest.*\.js$/, /\.html$/],
       // 排除携带指定参数的URL
-      // ignoreURLParametersMatching: [/^utm_/, /^fbclid$/]
+      // ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+      // 如果设置为true，则跳过等待，直接激活新的service worker。设置为false，则是利用message事件作为控制接口。
+      // skipWaiting: true,
     }
   }
 }
