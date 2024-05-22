37eae887c887731e5c45f9ca3a71583def8ee37f
yangxiaoping <2878847069@qq.com>
2024-05-22 19:23:39 +0800

    feat: skipWaiting At Once v2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <p>{{ `VUE_APP_TARGET = ${target}` }}</p>
-    <p>最终版自主控制更新策略——skipWaiting At Once</p>
+    <p>最终版自主控制更新策略——skipWaiting At Once v2</p>
     <div id="nav">
       <!-- <router-link :to="{ name: 'Home' }">Home</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link> |
