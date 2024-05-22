99f07a8968fb5990273d04ccfeda28aad7abe986
yangxiaoping <2878847069@qq.com>
2024-05-22 17:56:11 +0800

    chore: 测试navigate的skipWaiting是否也会重新加载相同资源

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // const pwaConfig = require('./versions/1.default/config')
-// const pwaConfig = require('./versions/2.navigate/config')
-const pwaConfig = require('./versions/3.html-cache/config')
+const pwaConfig = require('./versions/2.navigate/config')
+// const pwaConfig = require('./versions/3.html-cache/config')
 
 module.exports = {
   outputDir: 'dist',
