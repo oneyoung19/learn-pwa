8d528452e821404f6266643f0d7ccdd631d05827
oneyoung19 <37331643+oneyoung19@users.noreply.github.com>
2024-05-23 07:09:37 +0000

    Deploying to gh-pages from @ oneyoung19/learn-pwa@1a55cbab9d06198902387fbb05cf931eb1ae9ca2 🚀

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/learn-pwa/precache-manifest.24298f663e24dcdbf3b582b69311b878.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "learn-pwa"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.html?$/, new workbox.strategies.NetworkFirst({ "cacheName":"html-cache", plugins: [new workbox.expiration.Plugin({ maxEntries: 50, maxAgeSeconds: 2592000, purgeOnQuotaError: false })] }), 'GET');
