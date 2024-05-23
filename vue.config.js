d68c05a5382bc1a6042debf89e20c8795b3fe235
yangxiaoping <2878847069@qq.com>
2024-05-23 14:56:42 +0800

    feat: 支持sentry webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,25 @@
 // const pwaConfig = require('./versions/1.default/config')
 // const pwaConfig = require('./versions/2.navigate/config')
 const pwaConfig = require('./versions/3.html-cache/config')
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin')
 
 module.exports = {
+
   outputDir: 'dist',
   // outputDir: 'dist-InjectManifest',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/learn-pwa/'
     : '/',
-  ...pwaConfig
+  ...pwaConfig,
+  configureWebpack: {
+    devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-cheap-module-source-map',
+  },
+  chainWebpack: config => {
+    config.plugin('sentry')
+      .use(sentryWebpackPlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: 'oneyoung',
+        project: 'learn-pwa'
+      }))
+  }
 }
